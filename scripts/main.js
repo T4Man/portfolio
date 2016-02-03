b93415cf469bb8e56f5e22962f53bec9ff6f7b1f
t4man <timothywforman@gmail.com>
2016-02-02 21:57:15 -0800

    add articles.js, remove extraneous icons, add new feature

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,32 @@
 $(function(){
 
-  $('#audioStuff').hide();
-
-  $('#whiteGlobe').click(function(){
-    $('#audioStuff').fadeToggle(500);
+  $('#about').on('click', function(e){
+    e.preventDefault();
+    $('#my-projects').addClass("hidden");
+    if ($('#about-me').hasClass("hidden")){
+      $('#about-me').removeClass("hidden");
+    } else {
+      $('#about-me').addClass("hidden");
+    }
   });
 
-  $('.audioLinks').focus(function(){
-    $('#audioStuff').show();
+  $('#projects').on('click', function(e){
+    e.preventDefault();
+    $('#about-me').addClass("hidden");
+    if ($('#my-projects').hasClass("hidden")){
+      $('#my-projects').removeClass("hidden");
+    } else {
+      $('#my-projects').addClass("hidden");
+    }
   });
-  
+
+  $('#self-image').on('click', function(e){
+    e.preventDefault();
+    if ($('#contact').hasClass("hidden")){
+      $('#contact').removeClass("hidden");
+      } else {
+        $('#contact').addClass("hidden");
+      }
+  });
+
 });
