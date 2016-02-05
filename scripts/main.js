7680e8b31c1ce3cce58b118b2e6b3568917440b5
t4man <timothywforman@gmail.com>
2016-02-04 22:34:38 -0800

    add handlebars templating and more styling

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,49 @@
-$('#about-me').hide();
+var aboutArray = [];
+var projectArray = [];
 
-$('#about').on('click', function(){
-  $('#intro').hide();
-  $('#about-me').fadeIn();
+
+function ContentConstr (opts){
+  this.title = opts.title;
+  this.image = opts.image;
+  this.body = opts.body;
+}
+
+ContentConstr.prototype.populateAbout = function(){
+  var template = Handlebars.compile($('#about-me-template').text());
+  return template(this);
+};
+
+ContentConstr.prototype.populateProjects = function(){
+  var template = Handlebars.compile($('#projects-template').text());
+  return template(this);
+};
+
+AboutData.forEach(function(ele){
+  aboutArray.push(new ContentConstr(ele));
 });
 
-$('.icon-home').on('click', function(){
+ProjectData.forEach(function(ele){
+  projectArray.push(new ContentConstr(ele));
+});
+
+aboutArray.forEach(function(a){
+  $('#about-me').append(a.populateAbout());
+});
+
+projectArray.forEach(function(a){
+  $('#projects').append(a.populateProjects());
+});
+
   $('#about-me').hide();
-  $('#intro').fadeIn();
-});
+
+  $('#about').on('click', function(){
+    $('#projects').hide();
+    $('#about-me').fadeIn();
+  });
+
+  $('.icon-home').on('click', function(){
+    $('#about-me').hide();
+    $('#projects').fadeIn();
+  });
+
+
