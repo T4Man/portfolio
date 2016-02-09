00c614b442b44af4f81e5bc93e705645d4e14f76
t4man <timothywforman@gmail.com>
2016-02-08 21:16:50 -0800

    add contentView js file

--- scripts/contentView.js
+++ scripts/contentView.js
@@ -1,1 +1,20 @@
+contentView = {};
+
+contentView.initMainPage = function(){
+  ContentConstr.project.forEach(function(a){
+    $('#projects').append(a.populateProjects());
+  });
+};
+
+$('#about-me').hide();
+
+$('#about').on('click', function(){
+  $('#projects').hide();
+  $('#about-me').fadeIn();
+});
+
+$('.icon-home').on('click', function(){
+  $('#about-me').hide();
+  $('#projects').fadeIn();
+});
 
