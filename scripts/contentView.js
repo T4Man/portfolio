1d1b34ea74f69dda531079e4da8c752c213dbcf0
t4man <timothywforman@gmail.com>
2016-02-15 21:14:23 -0800

    add .empty to prevent duplicates

--- scripts/contentView.js
+++ scripts/contentView.js
@@ -4,20 +4,9 @@
 
   contentView.initMainPage = function(){
     ContentConstr.project.forEach(function(a){
+      $('#projects').empty();
       $('#projects').append(a.populateProjects());
     });
   };
-
-  $('#projects').hide();
-
-  $('#about').on('click', function(){
-    $('#projects').hide();
-    $('#about-me').fadeIn();
-  });
-
-  $('.icon-home').on('click', function(){
-    $('#about-me').hide();
-    $('#projects').fadeIn();
-  });
 
 })(window);
