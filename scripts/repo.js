036ebfe35f8196761c49da2d43099cbf5e31a138
t4man <timothywforman@gmail.com>
2016-02-18 21:29:34 -0800

    edit per_page condition

--- scripts/repo.js
+++ scripts/repo.js
@@ -2,19 +2,11 @@
   var repos = {};
   repos.all = [];
     repos.requestRepos = function(callback) {
-      $.ajax({
-        url: 'https://api.github.com/users/T4Man/repos?per_page=6&sort=updated',
-        type: 'GET',
-        headers: { 'Authorization': 'token ' + githubToken },
-        success: function(data, message, xhr) {
-          repos.all = [];
-          data.forEach(function(ele){
-            repos.all.push(ele);
-          });
-        }
-      }).done(function(){
-        if(callback) callback();
-      });
+      $.get('/github/users/T4Man/repos?per_page=5&sort=updated')
+    .done(function(data, message, xhr){
+      repos.all = data;
+    })
+    .done(callback);
   };
   repos.with = function(attr) {
     return repos.all.filter(function(repo) {
